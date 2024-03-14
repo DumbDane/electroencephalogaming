7b341584cb75be0f6b0e6599c3331dacb118c527
Chris Aftzidis <christina.aftzidis@gmail.com>
2024-03-14 09:53:37 +0100

    change data format

--- src/electroencephalogaming/inlet.py
+++ src/electroencephalogaming/inlet.py
@@ -28,6 +28,7 @@ def is_outlier(new_features, smpl_mean, smpl_sd, y):
     return False
 
 
+# should return a np.array of shape 6, with numbers that are voltages
 def get_features(): ...  # TODO how do we get individual features, can we maybe just read them out from the og data, remember to average over timeframe
 
 
@@ -46,7 +47,6 @@ def training(inlet_eeg: StreamInlet, inlet_psypy: StreamInlet):
     seen270 = 0
     initial_trained = False
 
-    # data_raw = {"eeg_data": [], "labels": []}
     labels = np.empty((NUM_TRIALS))
     c3_alpha = np.empty((NUM_TRIALS))
     c3_beta = np.empty((NUM_TRIALS))
@@ -109,9 +109,19 @@ def training(inlet_eeg: StreamInlet, inlet_psypy: StreamInlet):
         },
     }
 
+    features = np.array(
+        [
+            c3_alpha,
+            c3_beta,
+            cz_alpha,
+            cz_beta,
+            c4_alpha,
+            c4_beta,
+        ]
+    )
     print("now receiving data...")
     while True:
-        # TODO: do we need the timestamps and are we pushing them
+        # TODO: do something with the timestamps
         chunk_eeg, timestamps_eeg = inlet_eeg.pull_chunk()
         chunk_psypy, timestamps_psypy = (
             inlet_psypy.pull_chunk()
@@ -119,69 +129,42 @@ def training(inlet_eeg: StreamInlet, inlet_psypy: StreamInlet):
 
         new_x, new_y = chunk_eeg, chunk_psypy
 
-        # TODO: decide which one below is easier to work with, needs to be a dict or series of some sort
-        (
-            c3_alpha_new,
-            c3_beta_new,
-            cz_alpha_new,
-            cz_beta_new,
-            c4_alpha_new,
-            c4_beta_new,
-        ) = get_features()
         new_features = get_features()
-        # append to the series objects
-        features = [
-            c3_alpha,
-            c3_beta,
-            cz_alpha,
-            cz_beta,
-            c4_alpha,
-            c4_beta,
-        ]  # create list of series objects
         if is_outlier(new_features, smpl_mean, smpl_sd, new_y):
             # TODO: does this make sense for the first ten?
             print("this trial is an outlier and will not be considered")
             continue
-        # TODO increment num_samples_seen here and then append to list before calling update
+        num_samples_seen += 1
+        labels[num_samples_seen] = new_y
+        match new_y:
+            case "90":
+                seen90 += 1
+            case "180":
+                seen180 += 1
+            case "270":
+                seen270 += 1
+
+        for i in range(len(FEATURE_NAMES)):
+            features[i].append(new_features[i])
+
         smpl_mean, smpl_sd = update_mean_sd(
             features, smpl_mean, smpl_sd, num_samples_seen, labels
         )
 
         if not initial_trained:
-            # TODO once the classifier is trained we want to only keep the "best" data part, what do we do before?
-            data_raw["eeg_data"].append(features.values())
-            labels[num_samples_seen] = new_y
-            match new_y:
-                case "90":
-                    seen90 += 1
-                case "180":
-                    seen180 += 1
-                case "270":
-                    seen270 += 1
-            num_samples_seen += 1
             if all([seen90, seen180, seen270]) > 10:
                 clf = LinearDiscriminantAnalysis()
-                clf.fit(data_raw["eeg_data"], labels)
+                clf.fit(features.T, labels)
                 initial_trained = True
         else:
             # TODO: add a selection process that gets the best x value
-            data_raw["eeg_data"].append(features.values())
-            labels[num_samples_seen] = new_y
-            num_samples_seen += 1
-            match new_y:
-                case "90":
-                    seen90 += 1
-                case "180":
-                    seen180 += 1
-                case "270":
-                    seen270 += 1
             pred_y = clf.predict(new_x)
             # TODO: figure out if we can show this prediction on screen somehow or in general what to do with it
 
-            # supposedly, we recompute weights idk what that means for LDA
+            # TODO: add weights
             if all([seen90, seen180, seen270]) > 5:
                 clf = LinearDiscriminantAnalysis()
-                clf.fit(data_raw["eeg_data"], labels)
+                clf.fit(features.T, labels)
 
         sleep(0.1)
 
